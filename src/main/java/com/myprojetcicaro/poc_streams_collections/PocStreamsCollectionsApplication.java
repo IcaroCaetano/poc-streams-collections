d5497bdf35bd58c31860e59bdaf057ac37fe1882
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-12 09:39:44 -0300

    first commit

---
+++ src/main/java/com/myprojetcicaro/poc_streams_collections/PocStreamsCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.myprojetcicaro.poc_streams_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocStreamsCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocStreamsCollectionsApplication.class, args);
+	}
+
+}
