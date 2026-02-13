42e564692b9d1f3c798164424afa5d5b95290b2d
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-13 09:52:57 -0300

    feat: add map model

---
+++ src/main/java/com/myprojetcicaro/poc_streams_collections/map/User.java
@@ -0,0 +1,42 @@
+package com.myprojetcicaro.poc_streams_collections.map;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String role;
+    private double salary;
+
+    public User(Long id, String name, String role, double salary) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
