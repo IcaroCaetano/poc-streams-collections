db00e7ff91627a357b8c635697877c99c9a97a94
IcaroCaetano <icaro.caetano@gmail.com>
2026-02-20 22:42:52 -0300

    feat: add import java.util.EnumSet;

---
+++ src/main/java/com/myprojetcicaro/poc_streams_collections/enumset/EnumSetExamples.java
@@ -0,0 +1,72 @@
+package com.myprojetcicaro.poc_streams_collections.enumset;
+
+import java.util.EnumSet;
+
+public class EnumSetExamples {
+
+    public static void run() {
+        basicExample();
+        allOfExample();
+        rangeExample();
+        complementExample();
+        removeExample();
+    }
+
+    enum Permission {
+        READ,
+        WRITE,
+        EXECUTE,
+        DELETE,
+        SHARE
+    }
+
+    private static void basicExample() {
+        System.out.println("\n===EnumSet Básico ===");
+
+        EnumSet<Permission> permissions =
+                EnumSet.of(Permission.READ, Permission.WRITE);
+
+        permissions.forEach(System.out::println);
+    }
+
+    private static void allOfExample() {
+        System.out.println("\n=== EnumSet.allOf ===");
+
+        EnumSet<Permission> all =
+                EnumSet.allOf(Permission.class);
+
+        all.forEach(System.out::println);
+    }
+
+    private static void rangeExample() {
+        System.out.println("\n=== EnumSet.range ===");
+
+        EnumSet<Permission> range =
+                EnumSet.range(Permission.READ, Permission.DELETE);
+
+        range.forEach(System.out::println);
+    }
+
+    private static void complementExample() {
+        System.out.println("\n=== EnumSet.complementOf ===");
+
+        EnumSet<Permission> basic =
+                EnumSet.of(Permission.READ, Permission.WRITE);
+
+        EnumSet<Permission> complement =
+                EnumSet.complementOf(basic);
+
+        complement.forEach(System.out::println);
+    }
+
+    private static void removeExample() {
+        System.out.println("\n=== Remove ===");
+
+        EnumSet<Permission> permissions =
+                EnumSet.allOf(Permission.class);
+
+        permissions.remove(Permission.DELETE);
+
+        permissions.forEach(System.out::println);
+    }
+}
